08a5151136dcc0d1c1aab164304cfa1f44ce21f2
amulyadk29 <amulyadk49@gmail.com>
2025-12-10 17:24:27 -0800

    added new files

---
+++ pythnprgm.py
@@ -0,0 +1,55 @@
+#say hello 1
+# print("hello world")
+# print(546)
+# print("3421")
+
+# 2 do math
+
+# print(5+78)
+# print(32-87)
+# print(546*43)
+# print(54/4)
+# print(65%76)
+# print(56**5)
+# print(54//8)
+# print(3+4*7)
+# print((3+5)*3)
+#age challenge 
+# print(21*365)
+
+# teach ur code to remember things
+
+# color = "green"
+# age = 21
+# orange_flag = True
+# print("fav color",color)
+# print("age",age)
+# print("of",orange_flag)
+# birth_year = 2004
+# current_year = 2025
+
+# age = current_year - birth_year
+# print(age)
+
+# food = "pizza"
+# color = "green"
+# animal = "dolphin"
+
+# print("My favorite food is ", food, "my favorite color is ", color, " and I want a pet ", animal, "!")
+
+# name = "avatar"
+# species = "homo"
+# age = 23
+# print("my name is",name,"my species is",species,"my age",age)
+
+# age = int(input("what is ur age"))
+# print("my age is",age)
+# print(type(age))
+
+# age = 43 
+# future = int(age) + 4
+# print(future)
+
+year = input("tell us ur birth year:")
+age = 2025 - int(year)
+print("ur age is:",age)
