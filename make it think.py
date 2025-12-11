6c53eb0782e58687b579bd8cafe1309c7e8ad3a4
amulyadk29 <amulyadk49@gmail.com>
2025-12-11 07:25:31 +0530

    Deleted second and added files

---
+++ make it think.py
@@ -0,0 +1,75 @@
+# make it think 
+# age = 13
+# if age >= 18:
+#     print("adult")
+# else: 
+#     print("not adult")
+
+# i = int(input("tell me ur age:"))
+# if i >= 13:
+#   print("u can watch movie")
+# else:
+#   print("ur to young")
+
+# i = int(input("enter a num"))
+# print("ans is",i/2)
+
+# try:
+#     i = int(input("enter a num"))
+#     print("ans is",i/7)
+# except:
+#     print("try only nemeric data type")
+
+# for i in range(1,6,2):
+#     print(i)
+
+# i = 1
+# while i <= 10:
+#     print(i)
+#     i+=1
+
+# for i in range(3):
+#     print("Hi!")
+
+# Write your code here.
+# for i in range(5,0,-1):
+#     print(i)
+    
+
+# print("liftoff!")
+
+# contiunue
+# for i in range(6):
+#     if i==4:
+#         continue
+#     print(i)
+
+# break
+# while True:
+#     age = int(input("enter ur age:"))
+#     if age < 18:
+#         print("ur a kid")
+#         break
+#     print("ur ans",age)
+
+# while True:
+#     i = input("say anything casual talk:")
+#     if i == bye:
+#         break
+#     print("next word pls")
+
+# for i in range(1,11,2):
+#     print(i)
+
+# an = ["cat","pig","dog","cow"]
+# for i in an:
+#  print(i)
+# print(animals[3])
+# print(animals[-3])
+# animals.append("monkey")
+# animals.remove("cat")
+
+# Write your code here.
+l = ["pig","green","cricket"]
+for i in l:
+ print("i like",i)
